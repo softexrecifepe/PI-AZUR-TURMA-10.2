801cb4c526bafe3e474eda7a7813c607027ea421
theoVm <theovm2006@gmail.com>
2024-10-27 11:47:22 -0300

    Funcionalidade de mudar entre pessoa fisica e juridica no cadastro

---
+++ Projeto_Azur/src/components/Form/FormFisico.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Input from './Input'; 
+import './FormCadastro.css';
+import logo from '../../assets/cadastro.png'
+
+
+
+const FormFisico = ({pessoaEscolhida}, {isFisica}) => {
+  return (
+    <div className="form-wrapper">
+      <div className="form-image">
+      <img src={logo}/>
+      </div>
+
+      <div className="form-container">
+        <div className="form-header">
+          <h1>Cadastro</h1>
+          <p className='msgHeader'>Vamos preparar sua conta para você começar a usar!</p>
+        </div>
+
+        <div className="form-toggle">
+          <label>
+            <input
+              type="radio"
+              checked={isFisica}
+              onChange={() => pessoaEscolhida(false)}
+            />
+            Pessoa Jurídica
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={!isFisica}
+              onChange={() => pessoaEscolhida(true)}
+            />
+            Pessoa Física
+          </label>
+        </div>
+
+        <form className="form-fields">
+          <Input label="Email" type="email" placeholder="Digite seu email" />
+          <Input label="CPF" type="text" placeholder="Digite seu CPF" />
+          <Input label="Telefone" type="tel" placeholder="Digite seu telefone" />
+          <Input label="Senha" type="password" placeholder="Digite sua senha" />
+          <Input label="Confirmar Senha" type="password" placeholder="Confirme sua senha" />
+
+          <button type="submit" className="form-button">
+            Cadastrar
+          </button>
+        </form>
+
+        <div className="form-footer">
+          <p>Já tem uma conta? Faça seu <a href="/login">Login</a></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FormFisico;
