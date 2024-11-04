467bc4b19e6aea317a6988ca5e57a26840b4112f
Ruan Guedes <122304817+ruanpedroguedes@users.noreply.github.com>
2024-11-04 17:28:13 +0000

    mensagem de erro e sucesso finalizado no cadastro

--- Projeto_Azur/src/components/Form/FormFisico.jsx
+++ Projeto_Azur/src/components/Form/FormFisico.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import input from './Input';
 import './FormCadastro.css';
 import './Input.css';
 import logo from '../../assets/cadastro.png';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
 
 const FormFisico = ({ pessoaEscolhida, isFisica }) => {
-
-  const endpointPf = 'http://localhost:3000/pf'
+  const endpointPf = 'http://localhost:3000/pf';
   
   const [usuario, setUsuario] = useState([]);
   const [formData, setFormData] = useState({
@@ -18,24 +17,21 @@ const FormFisico = ({ pessoaEscolhida, isFisica }) => {
     telefone: "",
   });
 
-
-
-
   const handleSubmit = async () => {
     try {   
       const response = await axios.post(endpointPf, formData);  
-      setUsuario([...usuario,response.data]) 
-      console.log('Dados enviados com sucesso:', response.data);
-      
-
-      // localStorage.setItem('cadastro', JSON.stringify(formData));
+      setUsuario([...usuario, response.data]); 
+      toast.success("Cadastro realizado com sucesso!");
+      console.log("Cadastro realizado com sucesso");
     } catch (error) {
       console.error('Erro ao enviar os dados:', error);
+      toast.error("Erro ao realizar o cadastro."); 
     }
   };
 
   return (
     <div className="form-wrapper">
+      <ToastContainer /> 
       <div className="form-image">
         <img src={logo} alt="Cadastro" />
       </div>
@@ -67,25 +63,24 @@ const FormFisico = ({ pessoaEscolhida, isFisica }) => {
 
         <div className="form-fields">
           <div className='input-group'>
-
             <label>Email</label>
             <input type='email' placeholder='Digite seu email' value={formData.email} onChange={(e) =>
-              setFormData({...formData, email: e.target.value})
+              setFormData({ ...formData, email: e.target.value })
             } />
             
             <label>CPF</label>
-            <input type="text" name="cpf" placeholder="Digite seu CPF" value={formData.cpf}  onChange={(e) =>
-              setFormData({...formData, cpf: e.target.value}) 
+            <input type="text" name="cpf" placeholder="Digite seu CPF" value={formData.cpf} onChange={(e) =>
+              setFormData({ ...formData, cpf: e.target.value })
             }/>
 
             <label>Telefone</label>
             <input type="tel" name="telefone" placeholder="Digite seu telefone" value={formData.telefone} onChange={(e) =>
-              setFormData({...formData, telefone: e.target.value}) 
+              setFormData({ ...formData, telefone: e.target.value })
             } />
             
             <label>Senha</label>
             <input type="password" name="senha" placeholder="Digite sua senha" value={formData.senha} onChange={(e) =>
-              setFormData({...formData, senha: e.target.value}) 
+              setFormData({ ...formData, senha: e.target.value })
             } />
             
             <label>Confirmar Senha</label>
